fa32a6eac154da05df66943655a5a714ae93fd14
Salimli <asalim201@cable.comcast.com>
2020-08-20 12:32:14 -0400

    1. MongoDB deployment added. 2. Mongo db k8s service added 3. application now connected to mongo db runing in docker container

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,15 @@
+// Importing standart libraries
 import express from 'express'
 import {json} from 'body-parser'
+import mongoose from 'mongoose'
+
+// THe Application libraries
 import {currentUserRouter} from './routes/current-user'
 import {signoutRouter} from './routes/signout'
 import {signupRouter} from './routes/signup'
 import {signinRouter} from './routes/signin'
 import {errorHandler} from './middlewares/error-handler'
+
 // Exporting Error message to send when user tries to reach page that do not exists
 import {NotFoundError} from './errors/not-found-error'
 
@@ -20,12 +25,29 @@ app.use(signoutRouter);
 
 // If the defined routes matched create a new NotFoundError error.
 // Express will capture a new Error event and pass it to errorHandler.
-app.all('*',() => {
-    throw new NotFoundError();
+// app.all means that all methods will match (GET, POST, UPDATE)
+app.all('*',(req,res,next) => {
+    next(new NotFoundError());
 })
 
 app.use(errorHandler);
 
-app.listen(3000,()=>{
-    console.log("Started AUTH service on port 3000. I'm Running in GCLOUD! ")
-});
+const start = async () => {
+    try{
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+            });
+        console.log("Connected to the DB. ")
+        } catch (err) {
+            console.error(err)
+        }
+    
+    app.listen(3000,()=>{
+        console.log("Started AUTH service on port 3000. I'm Running in GCLOUD! ")
+    });
+}
+ 
+start()
+
